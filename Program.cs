219df2d02a188a72b5383a153665fe4f2a4b9788
Abhijit Achare <abhijit.achare@betwaygroup.com>
2024-06-18 12:38:54 +0200

    fix formatting

--- Program.cs
+++ Program.cs
@@ -3,30 +3,13 @@ namespace TestInvestec;
 
 class Program
 {
-
-
     static async Task Main(string[] args)
     {
-        //Work out what the top 3 highest scoring anagrams in this list of scrabble words: https://raw.githubusercontent.com/benjamincrom/scrabble/master/scrabble/dictionary.json
-        //Scoring is as follows:
-        //1 Point - A, E, I, L, N, O, R, S, T and U.
-        //2 Points - D and G.
-        //3 Points - B, C, M and P.
-        //4 Points - F, H, V, W and Y.
-        //5 Points - K.
-        //8 Points - J and X.
-        //10 Points - Q and Z.
-        //To be anagram, there must be at least 2 words that have the same letters in the list e.g. in the example below, far is the only word with an f in it, so can't be an anagram of any other word in the list
-        //The word 'aha' scores 6 as it is A = 1, H = 4 and A = 1 as does 'aah' as it is A = 1, H = 4 and A = 1 and they both have the same letters in them.
-        //Attempt to balance efficiency with clear code
-        //It's not an OO structuring question. You don't need to use interfaces.
-        //It should be a console app
-
         //var result = CalculateScore("aha");
         //Console.WriteLine($"Score for aha: {result}");
         
         //Read file
-        var wordList = await LoadWordListAsync("C:\\Users\\abhijit.achare\\source\\repos\\TestInvestecApp\\dictionary.json");
+        var wordList = await LoadWordListAsync("C:\\Users\\user1\\source\\repos\\TestInvestecApp\\dictionary.json");
            
         // Calculate scores for each word
         var wordScores = wordList.ToDictionary(word => word, CalculateScore);
@@ -56,7 +39,6 @@ class Program
             return null;
         }
     }
-
 
     private static int CalculateScore(string word)
     {
